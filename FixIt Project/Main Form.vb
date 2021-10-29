eb67406cee7a6fe1b9e0e785845f8e99173f29ea
yesusmigag <yesusmigag@gmail.com>
2021-10-29 15:35:46 -0400

    Add project files.

---
+++ FixIt Project/Main Form.vb
@@ -0,0 +1,12 @@
+﻿' Name:         FixIt Project
+' Purpose:      Allows the user to enter a name.
+' Programmer:   Jesus M Alvarado Gonzalez 05/25/2021
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        ''Close aplication 
+        Me.Close()
+
+    End Sub
+End Class
